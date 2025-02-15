cc7e5c4bc537df0579c3ae16184b9929e0729857
shia <nahsor2242195@gmail.com>
2025-02-15 19:04:05 +0900

    kadai_008

--- js/script.js
+++ js/script.js
@@ -31,24 +31,73 @@
 //     });
 // });
 
+// $(function() {
+//     $('#red').on('click', function() {
+//         $('#target').css('color', 'red');
+//     });
+
+//     $('#blue').on('click', function() {
+//         $('#target').css('color', 'blue');
+//     });
+
+//     $('#add').on('click', function(){
+//         $('#target').toggleClass('sample');
+//     });
+
+//     $('#remove').on('click', function() {
+//         if ($('#target').hasClass('sample')) {
+//             console.log('sample');
+//         }
+//     });
+// });
+
+// $(function() {
+//     const p = $('p').text('Jquery');
+//     p.append('<br>add content');
+
+//     let num = 0;
+//     $('#append').on('click', function() {
+//         num++;
+//         $('ul').append('<li>追加したコンテンツ'+ num + '</li>');
+//     })
+//     $('#remove2').on('click', function() {
+//         num--;
+//         $('li:last').remove();
+//         if( num < 0) {
+//             num = 0;
+//         }
+//     });
+//     $('#show').on('click', function() {
+//         $('ul').show();
+//     });
+//     $('#hide').on('click', function() {
+//         $('ul').hide();
+//     });
+// });
+
 $(function() {
-    $('#red').on('click', function() {
-        $('#target').css('color', 'red');
+    $('#check').on('click', function() {
+        console.log('name:' + $('[name="username"]').val());
+        console.log('gender:' + $('[name="gender"]:checked').val());
+        console.log('blood:' + $('[name="blood"]').val());
+        $('[name="hobby"]:checked').each(function() {
+            console.log('hobby:' + $(this).val());
+        });
     });
 
-    $('#blue').on('click', function() {
-        $('#target').css('color', 'blue');
-    });
+    $('[name="username"]').on("input", function() {
+        let input = $(this).val();
+        if (input) {
+            $('#check').prop('disabled', false);
+        } else {
+            $('#check').prop('disabled', true);
+        }
+    })
 
-    $('#add').on('click', function(){
-        $('#target').toggleClass('sample');
-    });
-
-    $('#remove').on('click', function() {
-        if ($('#target').hasClass('sample')) {
-            console.log('sample');
+    $('[name="hobby"]').on('change', function() {
+        if ($('[name="hobby"]:checked').length > 3) {
+            $(this).prop('checked', false);
+            alert('3つまで');
         }
     });
-
-
 });
